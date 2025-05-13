1993837e674c3050809082bf356380506bd6e175
Adhi Hargo <cadmus.sw@gmail.com>
2025-05-13 10:22:35 +0700

    Revamp functions listing matching version specs

--- src/launch.nim
+++ src/launch.nim
@@ -3,6 +3,7 @@ import std/os
 import std/osproc
 import std/sequtils
 import std/strutils
+import std/sugar
 import std/tables
 
 import args
@@ -18,24 +19,35 @@ proc getArgData(): ref ArgumentsData =
     stderr.writeLine("> Command line error: ", e.msg)
     quit(QuitFailure)
 
-proc getMatchingVersionOpts(
-    versionSpec: string, versionOpts: seq[string]
-): seq[string] {.inline.} =
-  return filter(
-    toOpenArray(versionOpts, 0, versionOpts.high),
-    proc(v: string): bool =
-      v.startsWith(versionSpec),
-  )
+proc getVersionTable(
+    versionSpec: string, tblPaths: OrderedTable[string, string]
+): OrderedTable[string, string] {.inline.} =
+  return
+    if versionSpec == "":
+      tblPaths
+    else:
+      collect(initOrderedTable()):
+        for k, v in tblPaths.pairs:
+          if k.startsWith(versionSpec):
+            {k: v}
 
-proc processVersionSpec(versionSpec: string, versionOpts: seq[string]): string =
-  if versionSpec == "":
-    result = versionOpts[versionOpts.high]
-  elif not (versionSpec in versionOpts):
-    let matchingVersionOpts = getMatchingVersionOpts(versionSpec, versionOpts)
-    if matchingVersionOpts.len > 0:
-      result = matchingVersionOpts[matchingVersionOpts.high]
+proc getVersionOpts(
+    versionSpec: string, tblPaths: OrderedTable[string, string]
+): seq[string] =
+  let ctxTblPaths: OrderedTable[string, string] = getVersionTable(versionSpec, tblPaths)
+  return ctxTblPaths.keys.toSeq
 
-proc processCommandStr(versionSpec: string, passedArgs: string, confData: ref ConfigData): string =
+proc getVersionSpec(
+    versionSpec: string, tblPaths: OrderedTable[string, string]
+): string =
+  let ctxTblPaths: OrderedTable[string, string] = getVersionTable(versionSpec, tblPaths)
+  for k in reversed(ctxTblPaths.keys.toSeq):
+    if fileExists(ctxTblPaths[k]):
+      return k
+
+proc processCommandStr(
+    versionSpec: string, passedArgs: string, confData: ref ConfigData
+): string =
   let
     binPath = confData.paths.getOrDefault(versionSpec)
     cmdSwitches = confData.switches.getOrDefault(versionSpec)
@@ -47,12 +59,11 @@ proc runApp() =
   let
     argData = getArgData()
     confData = readConfig(argData.configPath)
-    versionOpts = sorted(confData.paths.keys.toSeq)
+    versionOpts = getVersionOpts(argData.versionSpec, confData.paths)
 
   if argData.commandType == cmdList:
-    let matchingVersionOpts = getMatchingVersionOpts(argData.versionSpec, versionOpts)
     stderr.writeLine("> Blender versions registered:")
-    for v in matchingVersionOpts:
+    for v in versionOpts:
       stderr.writeLine("> -v:", v)
     quit(QuitSuccess)
 
@@ -60,7 +71,7 @@ proc runApp() =
     stderr.writeLine("> No available version specs, exiting")
     quit(QuitFailure)
 
-  var versionSpec = processVersionSpec(argData.versionSpec, versionOpts)
+  var versionSpec = getVersionSpec(argData.versionSpec, confData.paths)
   if versionSpec == "":
     var versionSpecOpts = join(versionOpts, ", ")
     stderr.writeLine("> Invalid version spec: ", argData.versionSpec)
